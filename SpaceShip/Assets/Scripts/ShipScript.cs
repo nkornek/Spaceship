721dd34f7b90e79b54e7b53a5b10f0af19adf44f
Winnie Song <w.jy.song@gmail.com>
2014-07-01 13:08:00 -0400

    SHIP VARIABLES

---
+++ SpaceShip/Assets/Scripts/ShipScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipScript : MonoBehaviour {
+
+	bool hasUpdated;
+
+	public float shipCompletion;
+	public int shipWater, shipOil, shipMetal, shipFood;
+	public GameObject shipBar;
+	public float shipCap, shipBarCap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateShip ();
+		switch (GameManager.instance.gameState) {
+		case GameVariableManager.GameState.BeginWeekUpdate:
+			if (!hasUpdated) {
+				//print ("New Week Update!");
+				//print (ownedResourceType);
+				hasUpdated = true;
+			}
+			break;
+		}
+
+		if (shipCompletion >= 1000){
+			Debug.Log("SHIP COMPLETE");
+		}
+	
+	}
+
+	void UpdateShip () {
+		shipCompletion = (shipWater + shipOil + shipMetal + shipFood) / shipCap * shipBarCap;
+
+		Vector3 shipBarScale = shipBar.transform.localScale;
+		shipBarScale.y = (float)shipCompletion / shipCap * shipBarCap;
+		shipBar.transform.localScale = shipBarScale;
+	}
+}
