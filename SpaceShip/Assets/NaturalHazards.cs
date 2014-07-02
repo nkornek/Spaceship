2604fd49f93339c4756af7916171642d750b7eb9
pakawakawak <patrick.ph.chan@gmail.com>
2014-07-02 11:23:17 -0400

    updated current script. made a possible randomizer that will determine how many players and which ones will be affected by disaster. still haven't figured out how to incorporate it with the hazards yet...

--- SpaceShip/Assets/NaturalHazards.cs
+++ SpaceShip/Assets/NaturalHazards.cs
@@ -5,27 +5,14 @@ public class NaturalHazards : MonoBehaviour {
 
 
 	public	Country []player;
-	public int totalPlayers;
+	public int totalPlayers; //how many countries
 	public int hazardOfTheWeek; // range 0-8, 0 meaning safe week
 	public int maxHarzardOfWeek;
 	public int playerAffected;
-	public int numOfPlayers;
+	public int numOfPlayers; //affected by the hazard
 	public string hazardName;
 
-	NaturalHazards(){
-		totalPlayers = 4;
-		hazardOfTheWeek = 0; // default, nothing happens
-		maxHarzardOfWeek = 8;
-		playerAffected = -1;//no one is affected.
-		numOfPlayers = 0; // no one is affected by anything yet
-		hazardName = " ";
-		player = new Country[totalPlayers];
 
-		for(int i = 0; i < player.Length; i++)
-		{
-			player[i] = new Country();
-		}
-	}
 
 
 	void setHazardOfTheWeek(){
@@ -48,10 +35,22 @@ public class NaturalHazards : MonoBehaviour {
 		case 5: hazardName = "Plague"; break;
 		case 6: hazardName = "Wildfire";break;
 		case 7: hazardName = "Flood";break;
+		case 8: hazardName = "Heatwave"; break;
+		case 9: hazardName = "Hurricane"; break;
+		case 10: hazardName = "Stagnant Water"; break;
+		case 11: hazardName = "Solar Flare"; break;
+		case 12: hazardName = "Contaminated Crops"; break;
+		case 13: hazardName = "Mass Looting"; break;
+		case 14: hazardName = "Riot"; break;
+		case 15: hazardName = "Revolt"; break;
+
+
 		default: hazardName = "Quiet"; break;
 		}
 	}
 
+	//need to figure out how to incorporate more than 1 random country to damage
+	/*
 	void HazardDamage(int i, Country p){
 		switch(i)
 		{
@@ -63,88 +62,125 @@ public class NaturalHazards : MonoBehaviour {
 		case 5: PlagueDamage(p); break;
 		case 6: WildFireDamage(p);break;
 		case 7: FloodDamage(p);break;
+		case 8: HeatwaveDamage(p); break;
+		case 9: HurricaineDamage(p); break;
+		case 10: StagnantWaterDamage(p); break;
+		case 11: SolarFlareDamage(p); break;
+		case 12: ContaminatedCropDamage(p); break;
+		case 13: MassLootDamage(p); break;
+		case 14: RiotDamage(p); break;
+		case 15: RevoltDamage(p); break;
 		default: ; break;
 		}
 	}
+*/
 
-	void TornadoDamage(Country p)
-	{
-		p.subPopulation(3);
-		p.subFood(4);
-		p.subArmy(5);
+	void TornadoDamage(Country r, Country c) //country loses 1/2 their food supply
+	{	
+		r.stockFood -= (r.stockFood/2);
+		c.stockFood -= (c.stockFood/2);
 	}
 
 	
-	void DroughtDamage(Country p)
-	{
-		p.subPopulation(10);
-		p.subFood(10);
+	void DroughtDamage(Country r, Country w) 
+	{//population, military, stockWater, stockOil, stockFood, stockMetal;
+		r.stockWater -= (r.stockWater/2); //for random country
+		//need to add modifier water prod country reduce. water prod. by 1/2 for 3 weeks
 	}
 
 	
-	void VolcanoDamage(Country p)
+	void VolcanoDamage(Country r1, Country r2)
 	{
-		p.subPopulation(5);
-		p.subFood(10);
-		p.subArmy(5);
-		p.subMetal (10);
-		p.subWater(30);
+		r2.stockMetal -= (r2.stockMetal/2);
+		r1.stockMetal -= (r1.stockMetal/2);
 	}
 
 	
-	void EarthQuakeDamage(Country p)
+	void EarthQuakeDamage(Country m) //only metal country
 	{
-		p.subPopulation(2);
-		p.subFood(2);
-		p.subArmy(2);
-		p.subMetal (30);
-		p.subWater(10);
-		p.subOil (20);
+		//modifier to add where country prod. only 1 metal per country. 
+	//ex: 100 ppl, prod. 100 metal
 	}
 
 	
-	void PlagueDamage(Country p)
+	void PlagueDamage(Country r1, Country r2, Country r3) // 3 random countries lose 1/2 population
 	{
-		p.subPopulation(30);
-		p.subArmy(30);
+		r1.population -= (r1.population/2);
+		r2.population -= (r2.population/2);
+		r2.population -= (r2.population/2);
 	}
 
-	void WildFireDamage(Country p){
-		p.subArmy (10);
-		p.subFood(20);
-		p.subOil(40);
-		p.subWater(30);
+	void WildFireDamage(Country r1, Country r2){
+		r1.stockWater -= (r1.stockWater/2);	
+		r2.stockWater -= (r2.stockWater/2);
 	}
 
 	
-	void FloodDamage(Country p)
+	void FloodDamage(Country r1, Country f)
 	{
-		p.subPopulation(5);
-		p.subFood(5);
-		p.subArmy(5);
-		p.subMetal (2);
-		p.subOil (5);
+		r1.stockFood -= (r1.stockFood/2); // random country loses 1/2 their food supply
+		//modifier: food prod. country will prod. 1/2 their amount of food for 3 weeks
 	}
+
+	void HeatwaveDamage(Country r1, Country m){
+		r1.stockMetal -= (r1.stockMetal/2);
+		//modifier: metal country produces 1/2 metal for 3 weeks.
+	}
+
+	void HurricaineDamage(Country r1, Country r2){
+		r1.stockOil -= (r1.stockOil/2);
+		r2.stockOil -= (r2.stockOil/2);
+	}
+
+	void StagnantWaterDamage(Country r){
+		//modifier: country produces nothing for 3 weeks
+	}
+
+	void SolarFlareDamage(Country f){
+		f.stockOil -= f.stockOil; //fuel country loses all their fuel
+		//modifier: fuel country doesn't produce anything for 3 weeks
+	}
+
+	void ContaminatedCropDamage(Country pr){
+		//country/countries that received food from Food prod country lose 1/3 their population
+		pr.population -= (pr.population/3);
+	}
+
+	void MassLootDamage(Country r1, Country r2, Country r3, Country r4){
+		//modifier: every country's exports are reduced to 0 for 3 weeks
+	}
+	void RiotDamage(Country r){
+		//modifier: 1 random country uses 1/5x their usual resource to sustain population
+		//for 3 weeks.
+	}
+
+	void ReVoltDamage(Country r1, Country r2){
+		//modifier: 2 random countries produce 2 resources/1 citizen for 4 weeks
+	}
+
 
 	void setAffectedPlayers()
 	{
-		int affectedPlayer = Random.Range (0, 4);
+		int affectedPlayer = Random.Range (0, 5);
 		setHazardOfTheWeek();
-		HazardDamage(hazardOfTheWeek, player[affectedPlayer]);
+		//need to figure out how to incorporate more than 1 player
+		//for the hazard damage.
+
+		//HazardDamage(hazardOfTheWeek, player[affectedPlayer]);
 	}
 
-	/*
-	//algorithm to make sure each array has unique value for more (number of people between 2-3) 
-	//is too time consuming... skip.
 
-	public int [] setEffectedPlayers(int nOfPl){
+	//algorithm to make sure each array has unique value for more (number of people between 2-3) 
+	//hasn't been tested yet.
+	/*
+	public int [] determineAffectedPlayers(int nOfPl){
 
 		int []ppl;
 		int num = -1;
-		int [] pplIndex = {0,1,2,3};
+		bool [] pplIndex = {false,false,false,false};
 		if(nOfPl > 0 && nOfPl != 4)
 		{
-			num = Random.Range(0,4);
+			num = Random.Range(1,4);
 			ppl = new int[ num ];
 
 			for(int i = 0; i < ppl.Length; i++)
@@ -155,21 +191,39 @@ public class NaturalHazards : MonoBehaviour {
 			//check if there are duplicate numbers
 			for(int i = 0; i < pplIndex.Length; i++)
 			{
-				for(int j = 0; j < ppl.Length; j++)
+					   while(pplIndex[ppl[i]] == true){
+							ppl[i] = Random.Range(0,4);
+					}
+
+				if(pplIndex[ppl[i]] == false)
 				{
-					if(ppl[j] == pplIndex[i]) 
+					pplIndex[ppl[i]] = true;
 				}
+
 			}
 		}
 
 		return ppl;
 	}
-
 */
+
 
 	// Use this for initialization
 	void Start () {
 		Random.seed = 3123;
+
+			totalPlayers = 4;
+			hazardOfTheWeek = 0; // default, nothing happens
+			maxHarzardOfWeek = 13;
+			playerAffected = -1;//no one is affected.
+			numOfPlayers = 0; // no one is affected by anything yet
+			hazardName = " ";
+			player = new Country[totalPlayers];
+			
+			for(int i = 0; i < player.Length; i++)
+			{
+				player[i] = new Country();
+			}
 
 	
 	}
