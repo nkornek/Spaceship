72e41e348dfa6883b2e6543a1c75656e6ead9251
Li Zhi He <hlz1991@hotmail.com>
2014-07-02 11:14:33 -0400

    Laid out more foundation stuff

--- SpaceShip/Assets/Scripts/GameVariableManager.cs
+++ SpaceShip/Assets/Scripts/GameVariableManager.cs
@@ -8,7 +8,7 @@ public class GameVariableManager : MonoBehaviour {
 
 	//Fields
 	//The state of the game
-	public enum GameState { Menu, Crisis, BeginWeekUpdate, Management, Result }
+	public enum GameState { Menu, Crisis, BeginWeekUpdate, Management, Result, LookAtStar, TransferResources, AIReact, ResourceRecap }
 	//The type of the country object
 	public enum CountryType { FE, OF, UAT, RN }
 	//The type of resource that the country owns, that the country can harvest
