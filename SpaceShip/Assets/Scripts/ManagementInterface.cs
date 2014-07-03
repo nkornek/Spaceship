dcd937e695ffbc0f3c1005548a078983f041f35a
Li Zhi He <hlz1991@hotmail.com>
2014-07-03 09:36:42 -0400

    Fixed a small error with management interface

--- SpaceShip/Assets/Scripts/ManagementInterface.cs
+++ SpaceShip/Assets/Scripts/ManagementInterface.cs
@@ -12,44 +12,44 @@ public class ManagementInterface : MonoBehaviour {
 	public Country chosenCountry;
 
 	// Use this for initialization
-	void OnEnable () {
-		chosenCountry = GameObject.Find ("Player").GetComponent<PlayerScript>().country;
-		//reset stocks if sent is greater than stock
-		if (chosenCountry.GetComponent<Country>().sentFood > chosenCountry.stockFood)
-		{
-			chosenCountry.foodToShip = 0;
-			chosenCountry.foodToFE = 0;
-			chosenCountry.foodToOF = 0;
-			chosenCountry.foodToUAT = 0;
-			chosenCountry.foodToRN = 0;
-		}
-		if (chosenCountry.GetComponent<Country>().sentWater > chosenCountry.stockWater)
-		{
-			chosenCountry.waterToShip = 0;
-			chosenCountry.waterToFE = 0;
-			chosenCountry.waterToOF = 0;
-			chosenCountry.waterToUAT = 0;
-			chosenCountry.waterToRN = 0;
-		}
-		if (chosenCountry.GetComponent<Country>().sentMetal > chosenCountry.stockMetal)
-		{
-			chosenCountry.metalToShip = 0;
-			chosenCountry.metalToFE = 0;
-			chosenCountry.metalToOF = 0;
-			chosenCountry.metalToUAT = 0;
-			chosenCountry.metalToRN = 0;
-		}
-		if (chosenCountry.GetComponent<Country>().sentOil > chosenCountry.stockOil)
-		{
-			chosenCountry.oilToShip = 0;
-			chosenCountry.oilToFE = 0;
-			chosenCountry.oilToOF = 0;
-			chosenCountry.oilToUAT = 0;
-			chosenCountry.oilToRN = 0;
-		}
-
-	
-	}
+//	void OnEnable () {
+//		chosenCountry = GameObject.Find ("Player").GetComponent<PlayerScript>().country;
+//		//reset stocks if sent is greater than stock
+//		if (chosenCountry.GetComponent<Country>().sentFood > chosenCountry.stockFood)
+//		{
+//			chosenCountry.foodToShip = 0;
+//			chosenCountry.foodToFE = 0;
+//			chosenCountry.foodToOF = 0;
+//			chosenCountry.foodToUAT = 0;
+//			chosenCountry.foodToRN = 0;
+//		}
+//		if (chosenCountry.GetComponent<Country>().sentWater > chosenCountry.stockWater)
+//		{
+//			chosenCountry.waterToShip = 0;
+//			chosenCountry.waterToFE = 0;
+//			chosenCountry.waterToOF = 0;
+//			chosenCountry.waterToUAT = 0;
+//			chosenCountry.waterToRN = 0;
+//		}
+//		if (chosenCountry.GetComponent<Country>().sentMetal > chosenCountry.stockMetal)
+//		{
+//			chosenCountry.metalToShip = 0;
+//			chosenCountry.metalToFE = 0;
+//			chosenCountry.metalToOF = 0;
+//			chosenCountry.metalToUAT = 0;
+//			chosenCountry.metalToRN = 0;
+//		}
+//		if (chosenCountry.GetComponent<Country>().sentOil > chosenCountry.stockOil)
+//		{
+//			chosenCountry.oilToShip = 0;
+//			chosenCountry.oilToFE = 0;
+//			chosenCountry.oilToOF = 0;
+//			chosenCountry.oilToUAT = 0;
+//			chosenCountry.oilToRN = 0;
+//		}
+//
+//	
+//	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -60,6 +60,42 @@ public class ManagementInterface : MonoBehaviour {
 			countryPrompt2.gameObject.SetActive (true);
 			countryPrompt3.gameObject.SetActive (true);
 			background.enabled = endManagement.enabled = true;
+
+			//Resets resources to zero if there's not enough
+			chosenCountry = GameObject.Find ("Player").GetComponent<PlayerScript>().country;
+			//reset stocks if sent is greater than stock
+			if (chosenCountry.GetComponent<Country>().sentFood > chosenCountry.stockFood)
+			{
+				chosenCountry.foodToShip = 0;
+				chosenCountry.foodToFE = 0;
+				chosenCountry.foodToOF = 0;
+				chosenCountry.foodToUAT = 0;
+				chosenCountry.foodToRN = 0;
+			}
+			if (chosenCountry.GetComponent<Country>().sentWater > chosenCountry.stockWater)
+			{
+				chosenCountry.waterToShip = 0;
+				chosenCountry.waterToFE = 0;
+				chosenCountry.waterToOF = 0;
+				chosenCountry.waterToUAT = 0;
+				chosenCountry.waterToRN = 0;
+			}
+			if (chosenCountry.GetComponent<Country>().sentMetal > chosenCountry.stockMetal)
+			{
+				chosenCountry.metalToShip = 0;
+				chosenCountry.metalToFE = 0;
+				chosenCountry.metalToOF = 0;
+				chosenCountry.metalToUAT = 0;
+				chosenCountry.metalToRN = 0;
+			}
+			if (chosenCountry.GetComponent<Country>().sentOil > chosenCountry.stockOil)
+			{
+				chosenCountry.oilToShip = 0;
+				chosenCountry.oilToFE = 0;
+				chosenCountry.oilToOF = 0;
+				chosenCountry.oilToUAT = 0;
+				chosenCountry.oilToRN = 0;
+			}
 
 			if (endManagement.clicked) {
 				GameManager.instance.gameState = GameVariableManager.GameState.TransferResources;
