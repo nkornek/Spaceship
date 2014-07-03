cde80a14830d196ebff275154678e9ebbd1e8443
nkornek <nkornek@gmail.com>
2014-07-03 00:04:35 -0400

    Bringing up-to-date

--- SpaceShip/Assets/Scripts/ManagementInterface.cs
+++ SpaceShip/Assets/Scripts/ManagementInterface.cs
@@ -8,14 +8,52 @@ public class ManagementInterface : MonoBehaviour {
 	public Resource_alloc_country countryPrompt1, countryPrompt2, countryPrompt3;
 	public GUITexture background;
 	public GUI_Button endManagement;
+	public GameObject ship, C1, C2, C3;
+	public Country chosenCountry;
 
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
+		chosenCountry = GameObject.Find ("Player").GetComponent<PlayerScript>().country;
+		//reset stocks if sent is greater than stock
+		if (ship.GetComponent<Resource_alloc_ship>().sentFood > chosenCountry.stockFood)
+		{
+			chosenCountry.foodToShip = 0;
+			chosenCountry.foodToFE = 0;
+			chosenCountry.foodToOF = 0;
+			chosenCountry.foodToUAT = 0;
+			chosenCountry.foodToRN = 0;
+		}
+		if (ship.GetComponent<Resource_alloc_ship>().sentWater > chosenCountry.stockWater)
+		{
+			chosenCountry.waterToShip = 0;
+			chosenCountry.waterToFE = 0;
+			chosenCountry.waterToOF = 0;
+			chosenCountry.waterToUAT = 0;
+			chosenCountry.waterToRN = 0;
+		}
+		if (ship.GetComponent<Resource_alloc_ship>().sentMetal > chosenCountry.stockMetal)
+		{
+			chosenCountry.metalToShip = 0;
+			chosenCountry.metalToFE = 0;
+			chosenCountry.metalToOF = 0;
+			chosenCountry.metalToUAT = 0;
+			chosenCountry.metalToRN = 0;
+		}
+		if (ship.GetComponent<Resource_alloc_ship>().sentOil > chosenCountry.stockOil)
+		{
+			chosenCountry.oilToShip = 0;
+			chosenCountry.oilToFE = 0;
+			chosenCountry.oilToOF = 0;
+			chosenCountry.oilToUAT = 0;
+			chosenCountry.oilToRN = 0;
+		}
+
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (GameManager.instance.gameState == GameVariableManager.GameState.Management) {
 			shipPrompt.gameObject.SetActive (true);
 			countryPrompt1.gameObject.SetActive (true);
