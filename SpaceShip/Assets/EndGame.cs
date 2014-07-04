6eead0d1a0a19506e54c50d9e0ead6889a25b3d9
nkornek <nkornek@gmail.com>
2014-07-04 13:44:34 -0400

    started endgame interface

---
+++ SpaceShip/Assets/EndGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour {
+	public bool win, war;
+	public GUIText endText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (!win)
+		{
+
+		}
+	}
+}
